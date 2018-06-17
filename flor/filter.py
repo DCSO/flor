13687aba4b293b2c488f90321bd5d62e78bd7068
Andreas Dewes <andreas.dewes@7scientists.com>
2018-06-17 14:22:40 +0200

    Repaired merge damage.

--- flor/filter.py
+++ flor/filter.py
@@ -16,58 +16,58 @@ class BloomFilter(object):
         self.p = p
         self.n = n
         self.N = 0
-        self.data = data
         self.m = int(abs(math.ceil(float(n) * math.log(float(p)) / math.pow(math.log(2.0), 2.0))))
         #we work in 64 bit blocks as this is the format of the Go filter.
         self.M = int(math.ceil(float(self.m) / 64.0))*8
         self.k = int(math.ceil(math.log(2) * float(self.m) / float(n)))
-        self.bytes = bytearray([0 for i in range(self.M)])
+        self._bytes = bytearray([0 for i in range(self.M)])
+        self.data = data
 
     def __contains__(self, value):
         return self.check(value)
 
     def read(self, input_file):
-        bs4 = input_file.read(4)
-        if len(bs4) != 4:
+        bs8 = input_file.read(8)
+        if len(bs8) != 8:
             raise IOError("Invalid filter!")
-        self.n = unpack('<L', bs4)[0]
+        self.n = unpack('<Q', bs8)[0]
 
         bs8 = input_file.read(8)
         if len(bs8) != 8:
             raise IOError("Invalid filter!")
         self.p = unpack('<d', bs8)[0]
 
-        bs4 = input_file.read(4)
-        if len(bs4) != 4:
+        bs8 = input_file.read(8)
+        if len(bs8) != 8:
             raise IOError("Invalid filter!")
-        self.k = unpack('<L', bs4)[0]
+        self.k = unpack('<Q', bs8)[0]
 
-        bs4 = input_file.read(4)
-        if len(bs4) != 4:
+        bs8 = input_file.read(8)
+        if len(bs8) != 8:
             raise IOError("Invalid filter!")
-        self.m = unpack('<L', bs4)[0]
+        self.m = unpack('<Q', bs8)[0]
 
-        bs4 = input_file.read(4)
-        if len(bs4) != 4:
+        bs8 = input_file.read(8)
+        if len(bs8) != 8:
             raise IOError("Invalid filter!")
-        self.N = unpack('<L', bs4)[0]
+        self.N = unpack('<Q', bs8)[0]
 
         self.M = int(math.ceil(self.m/64.0))*8
 
-        self.bytes = bytearray(input_file.read(self.M))
-        if len(self.bytes) != self.M:
-            raise IOError("Mismatched number of bytes: Expected {}, got {}.".format(self.M,len(self.bytes)))
+        self._bytes = bytearray(input_file.read(self.M))
+        if len(self._bytes) != self.M:
+            raise IOError("Mismatched number of bytes: Expected {}, got {}.".format(self.M,len(self._bytes)))
 
-        #we read in any data that might be attached to the file
+        # we read any data that might be attached to the file
         self.data = input_file.read()
 
     def write(self, output_file):
-        output_file.write(pack('<L', self.n))
+        output_file.write(pack('<Q', self.n))
         output_file.write(pack('<d', self.p))
-        output_file.write(pack('<L', self.k))
-        output_file.write(pack('<L', self.m))
-        output_file.write(pack('<L', self.N))
-        output_file.write(bytes(self.bytes))
+        output_file.write(pack('<Q', self.k))
+        output_file.write(pack('<Q', self.m))
+        output_file.write(pack('<Q', self.N))
+        output_file.write(bytes(self._bytes))
         output_file.write(bytes(self.data))
 
     def add(self, value):
@@ -77,9 +77,9 @@ class BloomFilter(object):
             k = int(fpe / 8)
             l = fpe % 8
             v = 1 << l
-            if self.bytes[k] & v == 0:
+            if self._bytes[k] & v == 0:
                 new_value = True
-            self.bytes[k] |= v
+            self._bytes[k] |= v
         if new_value:
             self.N+=1
             if self.N >= self.n:
@@ -90,7 +90,7 @@ class BloomFilter(object):
         for fpe in fp:
             k = int(fpe / 8)
             l = fpe % 8
-            if self.bytes[k] & (1 << l) == 0:
+            if self._bytes[k] & (1 << l) == 0:
                 return False
         return True
 
